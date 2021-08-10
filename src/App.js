bfeed08b1378e2e782f489637cf3d24fbd0f4c0d
Manon Sainton <manonsainton.dev@gmail.com>
2021-08-10 18:19:09 -0400

    match inner Text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
   state = {
     button1: false,
     button2: false,
-    clickedButtons: []
+    clickedButtons: [],
+    matchedButtonName: [],
+    notifyAlert: false
   }
 
   removeChildren = (parentElement) => {
@@ -21,16 +23,19 @@ class App extends Component {
 
   testing = () => {
     let arr = ["one", "two", "three"];
-    
     setInterval((word) => {
       // console.log(word)
-      
   }, 3000);
   }
 
-  handleClick = (evt) => {
-    // debugger
- 
+  nonMatchAlert = () => {
+    this.setState({
+      notifyAlert: true
+    })
+    alert("Not a Match! Try Again")
+  }
+
+  handleClick = (evt) => { 
   this.state.clickedButtons.push(evt.target.parentElement)
   // console.log(evt)
   console.log(this.state.clickedButtons)
@@ -39,11 +44,9 @@ class App extends Component {
     // console.log(this.state.clickedButtons[0].className)
     if (this.state.clickedButtons[0].className === this.state.clickedButtons[1].className) {
       this.state.clickedButtons.forEach(div => this.removeChildren(div))
-      // this.removeChildren(div)
       this.setState({
         ...this.state, 
         clickedButtons: []
-
       })
     } else {
       this.setState({
@@ -54,41 +57,66 @@ class App extends Component {
     }
     
   }
+}
+
+  handleMatched = (evt) => {
+    console.log(evt)
+    this.state.matchedButtonName.push(evt.target.parentElement)
+    // debugger
+    if (this.state.matchedButtonName.length === 2) {
+        if (this.state.matchedButtonName[0].innerText === this.state.matchedButtonName[1].innerText) {
+          console.log("Yes!")
+          this.state.matchedButtonName.forEach(button => this.removeChildren(button))
+          this.setState({
+            ...this.state, 
+            matchedButtonName: [],
+            notifyAlert: false
+          })
+        } else {
+          this.nonMatchAlert() //repeat alert without refresh
+          this.setState({
+            ...this.state, 
+            clickedButtons: [],
+            notifyAlert: false
+          })
+        }
+    }
   }
 
 render () {
 
   return (
-    <div>
 <div>
 
-<div className="button1" >
-  <button onClick={this.handleClick} onMouseOver={this.testing}>1</button>
-</div>
+  <div>
 
-<div className="button2">
-  <button onClick={this.handleClick}>2</button>
-</div>
+    <div className="button1" >
+      <button onClick={this.handleClick} onMouseOver={this.testing}>1</button>
+    </div>
 
-</div>
+    <div className="button2">
+      <button onClick={this.handleClick}>2</button>
+    </div>
+
+  </div>
 
 
 <br></br>
-<div>
+  <div>
 
-<div className="button1">
-  <button onClick={this.handleClick} >Four</button>
-</div>
+    <div className="button1">
+      <button onClick={this.handleMatched} >Four</button>
+    </div>
 
-<div className="button2">
-  <button onClick={this.handleClick}>One</button>
-</div>
+    <div className="button2">
+      <button onClick={this.handleMatched}>One</button>
+    </div>
 
-<div className="button3">
-  <button onClick={this.handleClick}>One</button>
-</div>
+    <div className="button3">
+      <button onClick={this.handleMatched}>One</button>
+    </div>
 
-</div>
+  </div>
 
 </div>
 
